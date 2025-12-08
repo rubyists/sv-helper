f2d613c1dc67b0ff710524c488eab69cdadeba67
Tj (bougyman) Vanderpoel <bougyman@users.noreply.github.com>
2025-12-08 14:08:43 -0600

    fix: Correct the permissions of ./main when it is a directory

--- rsvlog.sh
+++ rsvlog.sh
@@ -18,11 +18,11 @@
 # CURRENT_LOG_FILE=filename.log (default 'current')
 
 set -e
-if [ $0 != "./run" ]; then
+if [ "$0" != "./run" ]; then
 	echo "This script meant to be linked as ./run in a service/log directory only!"
 	exit 1
 fi
-curdir=$(basename $(pwd))
+curdir=$(basename "$(pwd)")
 if [ "$curdir" != "log" ]; then
 	echo "This script meant to be run from a service/log directory only!"
 	exit 1
@@ -35,36 +35,48 @@ fi
 if [ -n "$SV_LOG_SYSLOG" ]; then
 	prio=${SV_LOG_SYSLOG_PRIORITY:-daemon.info}
 	echo "Logging to Syslog with priority ${prio}"
-	exec logger -p $prio
+	exec logger -p "$prio"
 fi
 
 if [ "x$SV_LOGDIR" != "x" ]; then
 	logdir=$SV_LOGDIR
 fi
 
-if [ -w /var/log ]; then
+if [ -d ./main ]
+then
+	usergroup=$(stat -c "%U:%G" ./main)
+	if [ "$usergroup" != "$user_group" ]; then
+		chown -R "$user_group" ./main
+    fi
+    echo "Logging in existing $PWD/main directory"
+    exec svlogd -t ./main
+fi
+
+if [ -w /var/log ]
+then
 	user_group=${USERGROUP:-rsvlog:adm}
 	if [ "x$logdir" = "x" ]; then
-		logdir=$(basename $(dirname $(pwd)))
+		logdir=$(basename "$(dirname "$(pwd)")")
 	fi
 	[ -d "/var/log/$logdir" ] || mkdir -p "/var/log/$logdir"
-	[ -L ./main ] || [ -d ./main ] || ln -s "/var/log/$logdir" ./main
-	[ -L ./current ] || ln -s main/current
+	ln -svf "/var/log/$logdir" ./main
+	[ -L ./current ] || ln -s main/current current
 	if [ "x$CURRENT_LOG_FILE" != "x" ]; then
 		[ -L "/var/log/$logdir/$CURRENT_LOG_FILE" ] || ln -s current "/var/log/$logdir/$CURRENT_LOG_FILE"
 	fi
 	usergroup=$(stat -c "%U:%G" "/var/log/$logdir")
 	if [ "$usergroup" != "$user_group" ]; then
-		chown -R $user_group "/var/log/$logdir"
+		chown -R "$user_group" "/var/log/$logdir"
 	fi
-	echo Logging as $user_group to /var/log/$logdir
-	exec chpst -u $user_group svlogd -t ./main
-else
-	echo Logging in $PWD
-	if [ "x$CURRENT_LOG_FILE" != "x" ]; then
-		[ -L "$CURRENT_LOG_FILE" ] || ln -s current "$CURRENT_LOG_FILE"
-	fi
-	exec svlogd -t ./
+	echo "Logging as $user_group to /var/log/$logdir"
+	exec chpst -u "$user_group" svlogd -t ./main
 fi
+
+echo "Logging in $PWD"
+
+if [ "x$CURRENT_LOG_FILE" != "x" ]; then
+    [ -L "$CURRENT_LOG_FILE" ] || ln -s current "$CURRENT_LOG_FILE"
+fi
+exec svlogd -t ./
 
 # vim: set noet ts=8 sw=8 sts=8
